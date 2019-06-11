72643d06fa5b4fc96253826c5f89be8c4fbc7919
Tony Baker <this.tonybaker@gmail.com>
2019-06-11 22:55:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "/multiply/:num1/:num2"
+  get "/multiply/:num1/:num2" do
   @addnumbers = params[:num1] + params[:num2]
   end
 
